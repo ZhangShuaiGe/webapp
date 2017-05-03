c1de3b9b55dff0e72d2ab3a70d290b38d8c275f9
zhangshuaige <1071296726@qq.com>
2017-05-03 23:24:41 +0800

    下拉刷新 打开新页面 用 open方法

---
+++ js/all.js
@@ -0,0 +1,8 @@
+var a = null;
+	a += '<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />';
+    a += '<link href="../css/mui.min.css" rel="stylesheet" />';
+    a += 'http://libs.baidu.com/jquery/2.1.4/jquery.min.js'
+    a += '<script src="../js/mui.min.js"></script>';
+    a += '<script src="../js/shipei.js" type="text/javascript" charset="utf-8"></script>';
+document.getElementsByTagName("head")[0].innerHTML += a;
+//$("head").append(a);
